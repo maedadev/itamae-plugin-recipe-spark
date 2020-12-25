8f239ff41d4bfeac455c98132784e5152c14bf9a
ichylinux <ichylinux@gmail.com>
2020-12-25 17:39:11 +0900

    Spark-2.4.7

--- lib/itamae/plugin/recipe/spark/version.rb
+++ lib/itamae/plugin/recipe/spark/version.rb
@@ -2,12 +2,12 @@ module Itamae
   module Plugin
     module Recipe
       module Spark
-        VERSION = "0.1.2"
+        VERSION = "0.1.3"
         
         SPARK_VERSION = [
           SPARK_VERSION_MAJOR = '2',
           SPARK_VERSION_MINOR = '4',
-          SPARK_VERSION_REVISION = '6'
+          SPARK_VERSION_REVISION = '7'
         ].join('.')
 
       end
