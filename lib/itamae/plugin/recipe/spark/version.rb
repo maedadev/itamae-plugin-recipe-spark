82cfd43cf0a6a6a1e31ece9997bbf34678432ab7
y-kumazaki <kumazaki@lab.acs-jp.com>
2021-04-09 10:19:22 +0900

    add support for spark-3.0.1

--- lib/itamae/plugin/recipe/spark/version.rb
+++ lib/itamae/plugin/recipe/spark/version.rb
@@ -2,12 +2,12 @@ module Itamae
   module Plugin
     module Recipe
       module Spark
-        VERSION = "0.1.3"
+        VERSION = "0.1.4"
         
         SPARK_VERSION = [
-          SPARK_VERSION_MAJOR = '2',
-          SPARK_VERSION_MINOR = '4',
-          SPARK_VERSION_REVISION = '7'
+          SPARK_VERSION_MAJOR = '3',
+          SPARK_VERSION_MINOR = '0',
+          SPARK_VERSION_REVISION = '1'
         ].join('.')
 
       end
