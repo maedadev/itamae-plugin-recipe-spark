6a3b6acf7875bff6d41e57bfc3bf9881e4c5fb63
yuichiro.asahi <yuichiro.asahi@ulsystems.co.jp>
2024-04-17 17:38:43 +0900

    add support for spark-redshift_2.12-6.2.0-spark_3.4

--- lib/itamae/plugin/recipe/spark/version.rb
+++ lib/itamae/plugin/recipe/spark/version.rb
@@ -2,7 +2,7 @@ module Itamae
   module Plugin
     module Recipe
       module Spark
-        VERSION = "0.1.9"
+        VERSION = "0.1.10"
 
         SPARK_VERSION = [
           SPARK_VERSION_MAJOR = '3',
@@ -10,12 +10,13 @@ module Itamae
           SPARK_VERSION_REVISION = '1'
         ].join('.')
 
-        SPARK_REDSHIFT_VERSION = '2.12-5.0.3'
-        SPARK_AVRO_VERSION = '2.12-3.0.1'
+        SPARK_REDSHIFT_VERSION = '2.12-6.2.0-spark_3.4'
+        SPARK_AVRO_VERSION = '2.12-3.4.2'
         MINIMAL_JSON_VERSION = '0.9.4'
-        REDSHIFT_JDBC_VERSION = '2.1.0.17'
+        REDSHIFT_JDBC_VERSION = '2.1.0.24'
         FASTDOUBLEPARSER_VERSION = '0.8.0'
         JETS3T_VERSION = '0.9.4'
+        AWS_SECRETSMANAGER_JDBC_VERSION = '1.0.12'
       end
     end
   end
